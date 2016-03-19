5918bfe14d93e6d1c61e0fe74e4efd41b17193f2
Luan Felix <luanfc1@gmail.com>
2016-03-19 13:34:39 -0300

    Check se o serviço esta ativo

---
+++ check-service.ps1
@@ -0,0 +1,15 @@
+﻿### CENTRALIT | EBSERH ####
+### Contato: luan.coelho@centralit.com.br
+
+
+$StatusService = Get-Service -Name AdobeARMservice
+
+    if ($StatusService.Status -ne "Running"){
+
+    Start-Service AdobeARMservice
+    Write-Host "O serviço foi ativo"
+    }
+
+    if ($StatusService.Status -eq "Running"){ 
+    Write-Host "O serviço ja esta ativo"
+    }
