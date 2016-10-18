12f25c973a6addd0e2eac91514fcdeb0ba55297e
luanfelix <luanfelix@me.com>
2016-10-18 14:13:21 -0200

    Alteração nome do server

--- exchange-new-session.ps1
+++ exchange-new-session.ps1
@@ -1,4 +1,4 @@
 ﻿# Abrir sessão remota no servidor Exchange via PowerShell
 
- $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://sede-vp-exmbx1.ebserhnet.ebserh.gov.br/PowerShell/ -Authentication Kerberos
+ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server.com.br/PowerShell/ -Authentication Kerberos
  Import-PSSession $Session
