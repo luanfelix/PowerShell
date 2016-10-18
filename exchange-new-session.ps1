54bc27af72601d72331be71f1f3fcf4b7b1a62d5
luanfelix <luanfelix@me.com>
2016-10-18 14:13:35 -0200

    Delete

--- exchange-new-session.ps1
+++
@@ -1,4 +0,0 @@
-﻿# Abrir sessão remota no servidor Exchange via PowerShell
-
- $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server.com.br/PowerShell/ -Authentication Kerberos
- Import-PSSession $Session
