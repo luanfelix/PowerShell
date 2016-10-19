a2ebea9680017f954135a5b6fa6efdfeb77c5d0a
luanfelix <luanfelix@me.com>
2016-10-19 16:24:41 -0200

    Teste

--- HealthDCs.ps1
+++ HealthDCs.ps1
@@ -1,4 +1,4 @@
-﻿###################################################################
+###################################################################
 #
 # Autor: Luan Felix
 # e-mail: luanfc1@gmail.com
@@ -24,10 +24,10 @@ foreach ($DC in $DCServers)
 {
 
 $Identity = $DC
-           
+
 $ParametersDatabase = New-Object -TypeName PSObject
 
-$ParametersDatabase | Add-Member -MemberType NoteProperty -Name server -Value $Identity.server      
+$ParametersDatabase | Add-Member -MemberType NoteProperty -Name server -Value $Identity.server
 
 if ( Test-Connection -ComputerName $DC -Count 1 -ErrorAction SilentlyContinue ) {
 
@@ -43,33 +43,33 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 if($serviceStatus.state -like "Running")
                 {
                 $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NetlogonService -Value "Netlogon Timeout"
-         
+
                  stop-job $serviceStatus
                 }
                 else
                 {
                 $serviceStatus1 = Receive-job $serviceStatus
                  if ($serviceStatus1.status -eq "Running") {
- 		   
-         	   $svcName = $serviceStatus1.name 
-         	   $svcState = $serviceStatus1.status          
+
+         	   $svcName = $serviceStatus1.name
+         	   $svcState = $serviceStatus1.status
          	   $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NetlogonService -Value $svcState.NetlogonService
-          
+
                   }
-                 else 
-                  { 
-       		  
-         	  $svcName = $serviceStatus1.name 
-         	  $svcState = $serviceStatus1.status          
-         	  $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NetlogonService -Value "Falha Serviço" 
-                  } 
+                 else
+                  {
+
+         	  $svcName = $serviceStatus1.name
+         	  $svcState = $serviceStatus1.status
+         	  $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NetlogonService -Value "Falha Serviço"
+                  }
                 }
  ##NTDS Service Status##
 		        $serviceStatus = start-job -scriptblock {get-service -ComputerName $($args[0]) -Name "NTDS" -ErrorAction SilentlyContinue} -ArgumentList $DC
                 wait-job $serviceStatus -timeout $timeout
                 if($serviceStatus.state -like "Running")
                 {
-                 
+
                  $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NTDS -Value "NTDS Timeout"
                  stop-job $serviceStatus
                 }
@@ -77,20 +77,20 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 {
                 $serviceStatus1 = Receive-job $serviceStatus
                  if ($serviceStatus1.status -eq "Running") {
- 		    
-         	   $svcName = $serviceStatus1.name 
-         	   $svcState = $serviceStatus1.status          
-         	   $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NTDS -Value $svcState.NTDS 
+
+         	   $svcName = $serviceStatus1.name
+         	   $svcState = $serviceStatus1.status
+         	   $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NTDS -Value $svcState.NTDS
                   }
-                 else 
-                  { 
-       		  
-         	  $svcName = $serviceStatus1.name 
-         	  $svcState = $serviceStatus1.status          
-         	  $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NTDS -Value "Falha Serviço" 
-                  } 
+                 else
+                  {
+
+         	  $svcName = $serviceStatus1.name
+         	  $svcState = $serviceStatus1.status
+         	  $ParametersDatabase | Add-Member -MemberType NoteProperty -Name NTDS -Value "Falha Serviço"
+                  }
                 }
- 
+
 ##DNS Service Status##
 		        $serviceStatus = start-job -scriptblock {get-service -ComputerName $($args[0]) -Name "DNS" -ErrorAction SilentlyContinue} -ArgumentList $DC
                 wait-job $serviceStatus -timeout $timeout
@@ -104,23 +104,23 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 {
                 $serviceStatus1 = Receive-job $serviceStatus
                  if ($serviceStatus1.status -eq "Running") {
- 
-         	   $svcName = $serviceStatus1.name 
-         	   $svcState = $serviceStatus1.status          
-         	   $ParametersDatabase | Add-Member -MemberType NoteProperty -Name DNS -Value $svcState.DNS 
+
+         	   $svcName = $serviceStatus1.name
+         	   $svcState = $serviceStatus1.status
+         	   $ParametersDatabase | Add-Member -MemberType NoteProperty -Name DNS -Value $svcState.DNS
                   }
-                 else 
-                  { 
-  
-         	  $svcName = $serviceStatus1.name 
-         	  $svcState = $serviceStatus1.status          
+                 else
+                  {
+
+         	  $svcName = $serviceStatus1.name
+         	  $svcState = $serviceStatus1.status
          	  $ParametersDatabase | Add-Member -MemberType NoteProperty -Name DNS -Value "Falha DNS"
-                  } 
+                  }
                 }
 
 
 ##Netlogons status##
-               add-type -AssemblyName microsoft.visualbasic 
+               add-type -AssemblyName microsoft.visualbasic
                $cmp = "microsoft.visualbasic.strings" -as [type]
                $sysvol = start-job -scriptblock {dcdiag /test:netlogons /s:$($args[0])} -ArgumentList $DC
                wait-job $sysvol -timeout $timeout
@@ -146,7 +146,7 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 }
 
 ##Replications status##
-               add-type -AssemblyName microsoft.visualbasic 
+               add-type -AssemblyName microsoft.visualbasic
                $cmp = "microsoft.visualbasic.strings" -as [type]
                $sysvol = start-job -scriptblock {dcdiag /test:Replications /s:$($args[0])} -ArgumentList $DC
                wait-job $sysvol -timeout $timeout
@@ -172,7 +172,7 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 }
 
 ##Services status##
-               add-type -AssemblyName microsoft.visualbasic 
+               add-type -AssemblyName microsoft.visualbasic
                $cmp = "microsoft.visualbasic.strings" -as [type]
                $sysvol = start-job -scriptblock {dcdiag /test:Services /s:$($args[0])} -ArgumentList $DC
                wait-job $sysvol -timeout $timeout
@@ -198,7 +198,7 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 }
 
 ##Advertising status##
-               add-type -AssemblyName microsoft.visualbasic 
+               add-type -AssemblyName microsoft.visualbasic
                $cmp = "microsoft.visualbasic.strings" -as [type]
                $sysvol = start-job -scriptblock {dcdiag /test:Advertising /s:$($args[0])} -ArgumentList $DC
                wait-job $sysvol -timeout $timeout
@@ -213,7 +213,7 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                $sysvol1 = Receive-job $sysvol
                if($cmp::instr($sysvol1, "passed test Advertising"))
                   {
-                  
+
                $ParametersDatabase | Add-Member -MemberType NoteProperty -Name advertising -Value "Advertising OK"
                   }
                else
@@ -224,8 +224,8 @@ $ParametersDatabase | Add-Member -MemberType NoteProperty -Name ping -Value Erro
                 }
 
                 $ParametersDatabases += $ParametersDatabase
-               
-                
-} 
+
+
+}
 
 $ParametersDatabases | Export-csv $healthdc -NoTypeInformation
